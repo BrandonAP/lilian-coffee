b65c948c289482356cf6078263ca96cc5688f56d
BrandonAP <barreguin842@gmail.com>
2020-05-06 13:24:33 -0500

    Update

---
+++ dashboard/javascript-files/script-toggle-sidebar-active.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.burger-login-button').click(function(){
+        $('.all-navigation-sidebar').toggleClass('active');
+        $('.burger-login-button').toggleClass('toggle');
+    })
+})
